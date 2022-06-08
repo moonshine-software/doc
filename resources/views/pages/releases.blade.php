0626274342900ddcc2a77c98638ad4368844f91e
LT <leetoplayaz@yandex.ru>
2022-06-08 12:18:28 +0300

    belongsto new feature

--- resources/views/pages/releases.blade.php
+++ resources/views/pages/releases.blade.php
@@ -1,6 +1,10 @@
 <x-page title="Обновления" :sectionMenu="null">
 <ul class="list-disc my-4">
     <li class="text-purple my-2">
+        <a class="underline" href="https://github.com/lee-to/moonshine/releases/tag/1.3.0">v1.3.0</a>
+    </li>
+
+    <li class="text-purple my-2">
         <a class="underline" href="https://github.com/lee-to/moonshine/releases/tag/1.2.0">v1.2.0</a>
     </li>
 
