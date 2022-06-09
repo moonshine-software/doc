43a63492a887d532dc48f6d2d1367789027cc44d
LT <leetoplayaz@yandex.ru>
2022-06-09 12:51:40 +0300

    authorization

--- resources/views/pages/authorization/index.blade.php
+++ resources/views/pages/authorization/index.blade.php
@@ -15,6 +15,65 @@
     <code>$withPolicy</code>
 </x-p>
 
+<x-p>
+    Доступные методы Policy:
+</x-p>
+
+<x-ul :items="['viewAny', 'view', 'create', 'update', 'delete', 'massDelete', 'restore', 'forceDelete']"></x-ul>
+
+<x-code language="php">
+namespace App\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Leeto\MoonShine\Models\MoonshineUser;
+use App\Models\Post;
+
+class PostPolicy
+{
+    use HandlesAuthorization;
+
+    public function viewAny(MoonshineUser $user)
+    {
+        //
+    }
+
+    public function view(MoonshineUser $user, Post $model)
+    {
+        //
+    }
+
+    public function create(MoonshineUser $user)
+    {
+        //
+    }
+
+    public function update(MoonshineUser $user, Post $model)
+    {
+        //
+    }
+
+    public function delete(MoonshineUser $user, Post $model)
+    {
+        //
+    }
+
+    public function massDelete(MoonshineUser $user)
+    {
+        //
+    }
+
+    public function restore(MoonshineUser $user, Post $model)
+    {
+        //
+    }
+
+    public function forceDelete(MoonshineUser $user, Post $model)
+    {
+        //
+    }
+}
+</x-code>
+
 <x-code language="php">
 namespace Leeto\MoonShine\Resources;
 
