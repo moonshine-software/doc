61274ffd38bd0da575f41021a3671947a36788ed
LT <leetoplayaz@yandex.ru>
2022-12-15 23:01:56 +0300

    added tinyMce

--- resources/views/pages/fields/wysiwyg.blade.php
+++ resources/views/pages/fields/wysiwyg.blade.php
@@ -1,6 +1,11 @@
-<x-page title="WYSIWYG">
+<x-page title="WYSIWYG" :sectionMenu="[
+'Разделы' => [
+    ['url' => '#start', 'label' => 'Trix/CKEditor/Quill'],
+    ['url' => '#tinymce', 'label' => 'TinyMce'],
+]
+]">
 
-<x-p>Текстовый редактор с плагином trix/ckeditor/quill</x-p>
+<x-sub-title id="start">Trix/CKEditor/Quill</x-sub-title>
 
 <x-code language="php">
 use Leeto\MoonShine\Fields\WYSIWYG;
@@ -23,6 +28,53 @@ public function fields(): array
 </x-code>
 
 <x-image src="{{ asset('screenshots/wysiwyg.png') }}"></x-image>
+
+<x-sub-title id="tinymce">TinyMce</x-sub-title>
+
+<x-code language="php">
+use Leeto\MoonShine\Fields\TinyMce;
+
+//...
+public function fields(): array
+{
+    return [
+        TinyMce::make('Описание', 'description'),
+
+        // Более расширенные настройки
+
+        TinyMce::make('Text')
+            // Переопределить набор плагинов
+            ->plugins('anchor')
+            // Переопределить набор toolbar
+            ->toolbar('undo redo | blocks fontfamily fontsize')
+            //Для измнения имени автора для плагина tinycomments
+            ->commentAuthor('Danil Shutsky')
+            //Теги
+            ->mergeTags([
+                ['value' => 'tag', 'title' => 'Title']
+            ]),
+    ];
+}
+
+//...
+</x-code>
+
+<x-p>
+    Зарегистрируйтесь на <x-link link="https://www.tiny.cloud" target="_blank">Tiny.Cloud</x-link> и получите токен.
+    После добавьте его в конфиг <code>config/moonshine.php</code>
+</x-p>
+
+<x-code language="php">
+    //...
+    'tinymce' => [
+        'token' => 'YOUR_TOKEN'
+    ]
+    //...
+</x-code>
+
+<x-p>
+    Настройки <x-link link="https://github.com/UniSharp/laravel-filemanager" target="_blank">Laravel FileManager</x-link> располагаются <code>config/lfm.php</code>
+</x-p>
 
 <x-next href="{{ route('section', 'fields-code') }}">Код</x-next>
 
