1b5e00fb3011cf67821fdf4d86ddb29d4dc6200f
LT <leetoplayaz@yandex.ru>
2023-01-02 19:56:54 +0300

    Resource preview block

--- resources/views/pages/ru/advanced/dashboard.blade.php
+++ resources/views/pages/ru/advanced/dashboard.blade.php
@@ -1,4 +1,11 @@
-<x-page title="Dashboard">
+<x-page title="Dashboard" :sectionMenu="[
+    'Разделы' => [
+        ['url' => '#basic', 'label' => 'Основы'],
+        ['url' => '#resource-preview', 'label' => 'Блок с записями ресурса']
+    ]
+]">
+
+<x-sub-title id="basic">Основы</x-sub-title>
 <x-p>
     Кастомизация главной страницы админ. панели осуществляется через класс <code>app/MoonShine/Dashboard.php</code>.
 </x-p>
@@ -34,4 +41,37 @@ class Dashboard extends DashboardScreen
 }
 </x-code>
 
+<x-sub-title id="resource-preview">Блок с записями ресурса</x-sub-title>
+
+<x-p>
+    Иногда крайне удобно добавить на главную страницу таблицу с записями из ресурса для быстого доступа.
+    Для этого используйте <code>ResourcePreview</code>
+</x-p>
+
+<x-code language="php">
+namespace App\MoonShine;
+
+use App\Models\Article;
+use Leeto\MoonShine\Dashboard\DashboardBlock;
+use Leeto\MoonShine\Dashboard\DashboardScreen;
+use Leeto\MoonShine\Dashboard\ResourcePreview;
+
+class Dashboard extends DashboardScreen
+{
+    public function blocks(): array
+    {
+        return [
+            DashboardBlock::make([
+                ResourcePreview::make(
+                    new ArticleResource(), // Обязательный параметр с ресурсом MoonShine
+                    'Latest articles' // Опционально - заголовок блока
+                    Article::query()->where('active', true)->limit(2) // Опционально QueryBuilder
+                )
+            ])
+        ];
+    }
+}
+</x-code>
+
+<x-image src="{{ asset('screenshots/resource_preview.png') }}"></x-image>
 </x-page>
