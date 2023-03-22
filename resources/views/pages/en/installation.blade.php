82d1a0c733f81cf2467b9d94ca9025e7e5b0cc30
ilhm344 <117576293+ilhm344@users.noreply.github.com>
2023-03-22 22:52:49 +0500

    home+installation

---
+++ resources/views/pages/en/installation.blade.php
@@ -0,0 +1,90 @@
+<x-page title="Installation" :sectionMenu="[
+     'Sections' => [
+         ['url' => '#requirements', 'label' => 'Requirements'],
+         ['url' => '#composer', 'label' => 'Composer'],
+         ['url' => '#install', 'label' => 'Install'],
+         ['url' => '#config', 'label' => 'Configuration'],
+     ]
+]">
+
+<x-sub-title id="requirements">Requirements</x-sub-title>
+
+<x-p>
+     To use MoonShine, the following requirements must be met prior to installation:
+</x-p>
+
+<x-ul :items="['php >=8.0', 'laravel >= 9.0', 'composer']"></x-ul>
+<x-sub-title id="composer" hashtag="1">Composer</x-sub-title>
+
+<x-code language="shell">
+    composer require lee-to/moonshine
+</x-code>
+
+<x-sub-title id="install" hashtag="2">Installation</x-sub-title>
+
+<x-code language="shell">
+    php artisan moonshine:install
+</x-code>
+
+<x-moonshine::alert type="default" icon="heroicons.information-circle">
+     After execution, <code>config/moonshine.php</code> with basic settings will be added.
+     <x-link link="#config">More about the config file</x-link>
+</x-moonshine::alert>
+
+<x-moonshine::alert type="default" icon="heroicons.information-circle">
+     A directory with the administrative panel and resources will also be added - <code>app/MoonShine</code>.
+     <x-link link="{{ route('moonshine.custom_page', 'resources-index') }}">More about Resources</x-link>
+</x-moonshine::alert>
+
+<x-moonshine::alert type="default" icon="heroicons.information-circle">
+     It will also add MoonShineServiceProvider <code>App\Providers\MoonShineServiceProvider</code> where you need to register resources.
+     <x-link link="{{ route('moonshine.custom_page', 'resources-index') }}">More about Resources</x-link>
+</x-moonshine::alert>
+
+<x-sub-title hashtag="3">
+    Create an Administrator
+</x-sub-title>
+
+<x-code language="shell">
+    php artisan moonshine:user
+</x-code>
+
+<x-sub-title hashtag="4">
+   Resource registration and menu configuration
+</x-sub-title>
+
+<x-p>
+     To register new resources in MoonShine and form a menu, we need <code>app/Providers/MoonShineServiceProvider.php</code>
+</x-p>
+
+<x-code language="php">
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Leeto\MoonShine\MoonShine;
+use Leeto\MoonShine\Menu\MenuItem;
+use Leeto\MoonShine\Resources\MoonShineUserResource;
+
+class MoonShineServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        // [tl! focus:start]
+        app(MoonShine::class)->registerResources([
+            MenuItem::make('Admins', new MoonShineUserResource()),
+        ]);
+        // [tl! focus:end]
+    }
+}
+</x-code>
+
+<x-p>
+     In this example, we have added a menu item with panel administrators
+     <x-link link="{{ route('moonshine.custom_page', 'advanced-menu') }}">More about Menu</x-link>
+</x-p>
+
+<x-p>
+     Great! Now you can create and register sections of the future admin panels and get to work!
+     But don't forget to read the documentation all the way through!
+</x-p>
+</x-page>
