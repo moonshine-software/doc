063918923ae85138db41241194510c1a4cc8ed18
DissNik <nikushkin@mail.ru>
2023-04-12 16:50:29 +0500

    docs(fields): Image

--- resources/views/pages/en/fields/image.blade.php
+++ resources/views/pages/en/fields/image.blade.php
@@ -5,73 +5,30 @@
 </x-extendby>
 
 <x-p>
-    Before using the file field, make sure that a symbolic link is set to the storage directory
+    Everything is the same as <code>File</code>, only the display changes
 </x-p>
-
-<x-code language="shell">
-php artisan storage:link
-</x-code>
 
 <x-code language="php">
 use MoonShine\Fields\Image;
 
 //...
-
 public function fields(): array
 {
     return [
+        //...
         // [tl! focus:start]
-        Image::make('Avatar', 'avatar')
+        File::make('Thumbnail', 'thumbnail')
             ->dir('/') // The directory where the files will be stored in storage (by default /)
-            ->disk('public') // filesystems disk
+            ->disk('public') // Filesystems disk
             ->allowedExtensions(['jpg', 'gif', 'png']) // Allowable extensions
         // [tl! focus:end]
+        //...
     ];
 }
-
 //...
 </x-code>
 
-<x-p>
-    The <code>multiple</code> method is used to upload multiple files.
-</x-p>
-
-<x-code language="php">
-Image::make('Avatar', 'avatar')
-    ->multiple()
-</x-code>
-
-<x-moonshine::alert type="default" icon="heroicons.information-circle">
-    The field in the database must be of type text or json<br>.
-    You also need to add cast for eloquent model - json or array or collection.
-</x-moonshine::alert>
-
-<x-p>
-    If you download multiple files, you must use the <code>removable</code> method to be able to delete them later
-</x-p>
-
-<x-code language="php">
-Image::make('Avatar', 'avatar')
-    ->multiple()
-    ->removable() // [tl! focus]
-</x-code>
-
-<x-p>
-    If you want to exclude the possibility of downloading a file, use the method <code>disableDownload</code>.
-</x-p>
-
-<x-code language="php">
-Image::make('Avatar', 'avatar')
-    ->disableDownload() // [tl! focus]
-</x-code>
-
-<x-p>
-    If you want to keep the original file name from the client, use the <code>keepOriginalFileName</code> method
-</x-p>
-
-<x-code language="php">
-Image::make('Avatar', 'avatar')
-    ->keepOriginalFileName() // [tl! focus]
-</x-code>
+<x-image theme="light" src="{{ asset('screenshots/image.png') }}"></x-image>
+<x-image theme="dark" src="{{ asset('screenshots/image_dark.png') }}"></x-image>
 
 </x-page>
--- resources/views/pages/ru/fields/image.blade.php
+++ resources/views/pages/ru/fields/image.blade.php
@@ -5,74 +5,30 @@
 </x-extendby>
 
 <x-p>
-    Прежде чем использовать файловое поле, необходимо убедиться что на директорию storage
-    установлена символическая ссылка
+    Все то же самое как и <code>File</code>, меняется только отображение
 </x-p>
-
-<x-code language="shell">
-php artisan storage:link
-</x-code>
 
 <x-code language="php">
 use MoonShine\Fields\Image;
 
 //...
-
 public function fields(): array
 {
     return [
+        //...
         // [tl! focus:start]
-        Image::make('Аватар', 'avatar')
+        File::make('Thumbnail', 'thumbnail')
             ->dir('/') // Директория где будут хранится файлы в storage (по умолчанию /)
-            ->disk('public') // filesystems disk
+            ->disk('public') // Filesystems disk
             ->allowedExtensions(['jpg', 'gif', 'png']) // Допустимые расширения
         // [tl! focus:end]
+        //...
     ];
 }
-
 //...
 </x-code>
 
-<x-p>
-    Для загрузки нескольких файлов используется метод <code>multiple</code>
-</x-p>
-
-<x-code language="php">
-Image::make('Аватар', 'avatar')
-    ->multiple()
-</x-code>
-
-<x-moonshine::alert type="default" icon="heroicons.information-circle">
-    Поле в базе необходимо типа text или json<br>.
-    Также необходимо добавить cast для eloquent модели - json или array или collection.
-</x-moonshine::alert>
-
-<x-p>
-    При множественной загрузке файлов, для возможности их удаления в последующем, необходимо воспользоваться методом <code>removable</code>
-</x-p>
-
-<x-code language="php">
-Image::make('Аватар', 'avatar')
-    ->multiple()
-    ->removable() // [tl! focus]
-</x-code>
-
-<x-p>
-    Если необходимо исключить возможность скачивания файла воспользуйтесь методом <code>disableDownload</code>
-</x-p>
-
-<x-code language="php">
-Image::make('Аватар', 'avatar')
-    ->disableDownload() // [tl! focus]
-</x-code>
-
-<x-p>
-    Если необходимо сохранять оригинальное имя файла от клиента воспользуйтесь методом <code>keepOriginalFileName</code>
-</x-p>
-
-<x-code language="php">
-Image::make('Аватар', 'avatar')
-    ->keepOriginalFileName() // [tl! focus]
-</x-code>
+<x-image theme="light" src="{{ asset('screenshots/image.png') }}"></x-image>
+<x-image theme="dark" src="{{ asset('screenshots/image_dark.png') }}"></x-image>
 
 </x-page>
