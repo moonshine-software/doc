0f28bcdebbb39aec299340f27c70e2f7d7704727
DissNik <nikushkin@mail.ru>
2023-04-13 14:03:14 +0500

    docs(filters): BelongsToFilter

--- resources/views/pages/ru/filters/belongs_to.blade.php
+++ resources/views/pages/ru/filters/belongs_to.blade.php
@@ -1,10 +1,27 @@
-<x-page title="BelongsTo">
+<x-page title="BelongsToFilter">
 
-<x-p>Все те же методы и свойства, что и у поля <code>MoonShine\Fields\BelongsTo</code></x-p>
+<x-extendby :href="route('moonshine.custom_page', 'fields-belongs_to')">
+    BelongsTo
+</x-extendby>
 
-<x-p><code>MoonShine\Filters\BelongsToFilter</code></x-p>
+<x-code language="php">
+use MoonShine\Filters\BelongsToFilter;
+
+//...
+
+public function filters(): array
+{
+    return [
+        BelongsToFilter::make('Author', resource: 'name')
+            ->nullable()
+            ->canSee(fn() => auth('moonshine')->user()->moonshine_user_role_id === 1),
+    ];
+}
+
+//...
+</x-code>
+
+<x-image theme="light" src="{{ asset('screenshots/filter_belongs_to.png') }}"></x-image>
+<x-image theme="dark" src="{{ asset('screenshots/filter_belongs_to_dark.png') }}"></x-image>
 
 </x-page>
-
-
-
