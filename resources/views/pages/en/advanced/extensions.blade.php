d96419e2848db9aad244d3a939d2185f5767914a
z-takaev <zatakata.web@gmail.com>
2023-04-21 23:13:47 +0300

    docs(extensions): description custom actions

--- resources/views/pages/en/advanced/extensions.blade.php
+++ resources/views/pages/en/advanced/extensions.blade.php
@@ -1,12 +1,13 @@
 <x-page title="Extensions" :sectionMenu="[
     'Sections' => [
         ['url' => '#custom-field', 'label' => 'Custom field'],
+        ['url' => '#custom-action', 'label' => 'Custom action'],
         ['url' => '#vendors', 'label' => 'Packages'],
     ]
 ]">
 
 <x-p>
-    MoonShine provides opportunities to extend the basic functionality and write your own packages that improve the features. In this section we will give a list of such packages and an example with the creation of your own field
+    MoonShine provides opportunities to extend the basic functionality and write your own packages that improve the features. In this section, we will provide a list of such packages, an example with creating your own field and action.
 </x-p>
 
 
@@ -58,6 +59,74 @@ final class CKEditor extends Field
 <x-p>
     That's it!
 </x-p>
+
+<x-sub-title id="custom-action">Custom Action</x-sub-title>
+
+<x-p>
+    MoonShine comes with several built-in actions such as
+    <x-link :link="route('moonshine.custom_page', 'actions-export')">Export</x-link> и
+    <x-link :link="route('moonshine.custom_page', 'actions-import')">Import</x-link>
+    but you can also create your own custom actions.
+</x-p>
+
+
+<x-p>
+    To do this, you need to create a class that extends the MoonShine action class and define the handle method.
+</x-p>
+
+
+<x-code language="php">
+namespace App\MoonShine\Actions;
+
+use MoonShine\Actions\Action;
+
+class CustomAction extends Action
+{
+    public function handle(): mixed
+    {
+        // Code with the handler logic
+    }
+}
+</x-code>
+
+<x-p>
+    This is enough to display our custom action on the resource page.
+    However, let's take a look at what else we can define in our action class.
+</x-p>
+
+
+<x-code language="php">
+class CustomAction extends Action
+{
+    protected static string $view = 'view.custom'; // Custom blade mapping
+
+    protected bool $withQuery = true; // Whether to pass the entire current getQuery to the action's URL.
+
+    protected bool $inDropdown = false; // Display the button outside the dropdown menu.
+
+    protected ?string $icon = 'heroicons.outline.table-cells'; // Icon for the button.
+}
+</x-code>
+
+
+<x-p>
+    Next, register the action in the actions method of the resource where you want to display it.
+</x-p>
+
+<x-code language="php">
+public function actions(): array
+{
+    return [
+        CustomAction::make('Custom Action'),
+    ];
+}
+</x-code>
+
+
+<x-p>
+    That's it!
+</x-p>
+
 
 <x-sub-title id="vendors">Packages</x-sub-title>
 
