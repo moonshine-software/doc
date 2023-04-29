f3197c94c7752f9226eaa29c738318133a8dfb1a
DissNik <nikushkin@mail.ru>
2023-04-29 18:17:46 +0500

    docs(fields): morphTo

--- resources/views/pages/en/fields/morph_to.blade.php
+++ resources/views/pages/en/fields/morph_to.blade.php
@@ -6,11 +6,30 @@
 
 <x-p>Relationship field in laravel like MorphTo</x-p>
 
-<x-p> Same as <code>MoonShine\Fields\BelongsTo</code> only for MorphTo</x-p> relationships
+<x-p>Same as <code>MoonShine\Fields\BelongsTo</code> but for MorphTo relationships</x-p>
 
-<x-p><code>MoonShine\Fields\MorphTo</code></x-p>
+<x-code language="php">
+use MoonShine\Fields\MorphTo;
+
+//...
+
+public function fields(): array
+{
+    return [
+        MorphTo::make('Commentable')->types([
+            Article::class => 'title'
+        ]),
+    ];
+}
+//...
+</x-code>
+
+<x-moonshine::alert type="default" icon="heroicons.information-circle">
+    Required <code>types</code> method with available classes.<br/>
+    The key is a reference to the model, and the value is the field to display.
+</x-moonshine::alert>
+
+<x-image theme="light" src="{{ asset('screenshots/morph_to.png') }}"></x-image>
+<x-image theme="dark" src="{{ asset('screenshots/morph_to_dark.png') }}"></x-image>
 
 </x-page>
-
-
-
