02c7d9857bafceed6357ddcc84bae179e668ef28
Илья <@>
2023-05-01 20:35:48 +0500

    docs(concept): small changes in text

--- resources/views/pages/en/concept.blade.php
+++ resources/views/pages/en/concept.blade.php
@@ -1,21 +1,31 @@
 <x-page title="The concept" :sectionMenu="null">
     <x-p>
-    The idea of Moonshine project is to create a versatile and customizable admin panel for Laravel projects. Almost all serious projects require an admin panel and it takes a lot of time to create it. So I had the idea to make my own admin panel, but it's a huge job and in a few years I managed to create a working version for the most common tasks.
+        The idea of Moonshine project is to create a versatile and customizable admin panel for Laravel projects.
+        Almost all serious projects require an admin panel, and it takes a lot of time to create it.
+        So I had the idea to make my own admin panel, but it's a huge job,
+        and in a few years I managed to create a working version for the most common tasks.
     </x-p>
 
     <x-p>
-    Recently Laravel community in my CutCode community got an offer to create their own open source project, and I figured this was the right approach to make a good admin panel together by joining forces on a common cause.
+        Recently, the CutCode community dedicated to Laravel received a proposal to create our own open source project,
+        and I figured this was the right approach to make a good admin panel together
+        by joining forces on a common cause.
     </x-p>
 
     <x-p>
-    Everyone will benefit from the interaction - you will use a quality product that will always evolve.
+        Everyone will benefit from the interaction - you will use a quality product that will always evolve.
     </x-p>
 
     <x-p>
-    An active and vibrant community is the heart of the project. Therefore, the most active users will be encouraged. How to encourage? A preliminary idea on the form of encouragement is to create a system of donations. Donated money is distributed among the main development participants. Project MoonShine participates in the support program from JetBrains "Free License Programs" - active developers receive a license for products from JetBrains.
+        An active and vibrant community is the heart of the project. Therefore, the most active users will be encouraged.
+        A preliminary idea on the form of encouragement is to create a system of donations.
+        Donated money is distributed among the main development participants.
+        Project MoonShine participates in the support program from JetBrains "Free License Programs" -
+        active developers receive a license for products from JetBrains.
     </x-p>
 
     <x-p>
-    My goal is to make the project a place where developers will want to come back. Only together can we make a functional and comfortable machine! Together we will do more than one at a time!
+        My goal is to make the project a place where developers will want to come back.
+        Only together we can make a functional and comfortable machine! Together we will do more than alone!
     </x-p>
 </x-page>
