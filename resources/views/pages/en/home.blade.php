1f7089ddc4e7e4531fbefb418b0b811f1b6abae6
Илья <@>
2023-05-01 18:46:07 +0500

    docs(home): small punctuation fix

--- resources/views/pages/en/home.blade.php
+++ resources/views/pages/en/home.blade.php
@@ -8,15 +8,16 @@
          You are on an open source project dedicated to the "MoonShine" admin panel.
      </x-p>
      <x-p>
-         I've been working on it for several years, and have used it on dozens of small projects, constantly tweaking it.
+         I've been working on it for several years and have used it on dozens of small projects constantly tweaking it.
      </x-p>
      <x-p>
          What does moonshine mean? It's not exactly "moonlight" - my idea is the name "moonshine".
-         The term means the independent production of a drink in illegal conditions under the cover of night :).
-         So I developed this admin panel at night in my free time under the moonlight;)
+         The term means the self-production of a drink in illegal conditions under the cover of night :)
+         So I developed this admin panel at night in my free time under the moonlight ;)
      </x-p>
      <x-p>
-         Everything is already ready for use in your projects, documentation has been created describing the installation, configuration and features.
+         Everything is already ready for use in your projects,
+         documentation has been created describing the installation, configuration and features.
      </x-p>
      <x-p class="font-bold">
          I invite interested users to use and develop Moonshine together.
