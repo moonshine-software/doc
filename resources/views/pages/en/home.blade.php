db6134b1fad0ffac99d37452c99dfb2ac7e3f252
Илья <tankolom-x@yandex.ru>
2023-05-03 16:43:53 +0500

    docs(home): updated english translation

--- resources/views/pages/en/home.blade.php
+++ resources/views/pages/en/home.blade.php
@@ -5,14 +5,14 @@
     <x-sub-title>Hello Laravel user!</x-sub-title>
 
      <x-p>
-         You are on an open source project dedicated to the "MoonShine" admin panel.
+         Welcome to the open-source project dedicated to the "MoonShine" admin panel.
      </x-p>
      <x-p>
          I've been working on it for several years and have used it on dozens of small projects constantly tweaking it.
      </x-p>
      <x-p>
-         What does moonshine mean? It's not exactly "moonlight" - my idea is the name "moonshine".
-         The term means the self-production of a drink in illegal conditions under the cover of night :)
+         What does moonshine mean? It's not exactly "moonlight" - my idea is the "moonshine" concept.
+         The term means the independent production of a drink in illegal conditions under the cover of the night :)
          So I developed this admin panel at night in my free time under the moonlight ;)
      </x-p>
      <x-p>
