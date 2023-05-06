721854a9651a1ab46d35f2350568af38bbe4f343
Илья <tankolom-x@yandex.ru>
2023-05-06 20:08:44 +0500

    docs(id): fix updated english translation

--- resources/views/pages/en/fields/id.blade.php
+++ resources/views/pages/en/fields/id.blade.php
@@ -6,7 +6,7 @@
 
 <x-p>
     It is almost always present and will be hidden by default on the add/edit page.
-    If the primary key has a name other than id you have to specify its name as the first argument of the make method.
+    If the primary key has a name other than id you have to specify its name as the first argument of the <code>make</code> method.
 </x-p>
 
 <x-code language="php">
