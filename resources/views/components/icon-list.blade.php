b90e4065db77d519bec6d9af58e266ffd982b158
DissNik <nikushkin@mail.ru>
2023-05-21 12:39:21 +0500

    fix(icons): icons copy

--- resources/views/components/icon-list.blade.php
+++ resources/views/components/icon-list.blade.php
@@ -7,11 +7,11 @@
     <x-moonshine::grid>
         @foreach (glob($pattern) as $filename)
             <div class="col-span-4 sm:col-span-3 lg:col-span-2 2xl:col-span-1 text-center">
-                <x-moonshine::card class="flex flex-col items-center card-copy">
-                    <div @click.prevent="navigator.clipboard.writeText('{{ $prefix . basename($filename, ".blade.php") }}')">
+                <div @click.prevent="navigator.clipboard.writeText('{{ $prefix . basename($filename, ".blade.php") }}')">
+                    <x-moonshine::card class="flex flex-col items-center card-copy">
                         <x-moonshine::icon :icon='$prefix . basename($filename, ".blade.php")' size="8"/>
-                    </div>
-                </x-moonshine::card>
+                    </x-moonshine::card>
+                </div>
                 <small class=" text-xs leading-normal">{{ basename($filename, ".blade.php") }}</small>
             </div>
         @endforeach
