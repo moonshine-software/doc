840b86f5a45521a865dd3087b7f95038557ac000
DissNik <nikushkin@mail.ru>
2023-05-22 16:20:01 +0500

    docs(components): refactor Tabs

--- resources/views/pages/en/components/tabs.blade.php
+++ resources/views/pages/en/components/tabs.blade.php
@@ -6,6 +6,12 @@
 
 <x-code language="blade" file="resources/views/examples/components/tabs.blade.php"></x-code>
 
-@include("examples/components/tabs")
+<x-moonshine::grid>
+    <x-moonshine::column adaptiveColSpan="12" colSpan="6">
+        <x-moonshine::card>
+            @include("examples/components/tabs")
+        </x-moonshine::card>
+    </x-moonshine::column>
+</x-moonshine::grid>
 
 </x-page>
