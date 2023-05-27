e8836c7587385c2aee9beb24b5a0265adf383b04
Илья <tankolom-x@yandex.ru>
2023-05-27 12:18:52 +0500

    docs(metrics): updated english translation in index

--- resources/views/pages/en/metrics/index.blade.php
+++ resources/views/pages/en/metrics/index.blade.php
@@ -1,16 +1,16 @@
 <x-page title="Metrics">
 
 <x-p>
-    Blocks for displaying statistics
+    These blocks are intended for displaying statistics
 </x-p>
 
 <x-p>
-    You can display in on the home page <x-link link="{{ route('moonshine.custom_page', 'advanced-dashboard') }}">control panels</x-link>
-    and in each individual <x-link link="{{ route('moonshine.custom_page', 'resources-index') }}">at</x-link>
+    You can show them on the <x-link link="{{ route('moonshine.custom_page', 'advanced-dashboard') }}">admin panel</x-link> home page
+    and in every individual <x-link link="{{ route('moonshine.custom_page', 'resources-index') }}">resource</x-link> as well
 </x-p>
 
 <x-p>
-    Sef methods are also available to metrics
-    <x-link link="{{ route('moonshine.custom_page', 'decorations-layout#grid-column') }}">Layout (columnSpan)</x-link>
+    Metrics are also provided with
+    <x-link link="{{ route('moonshine.custom_page', 'decorations-layout#grid-column') }}">Layout (columnSpan)</x-link> decorator features
 </x-p>
 </x-page>
