6a958f8b450f4cf4729618107c136688fcb1b107
DissNik <nikushkin@mail.ru>
2023-05-28 13:44:35 +0500

    docs(decoration): add screenshot for block

--- resources/views/pages/ru/decorations/block.blade.php
+++ resources/views/pages/ru/decorations/block.blade.php
@@ -5,36 +5,43 @@
 </x-p>
 
 <x-code language="php">
-use MoonShine\Decorations\Block;
+use MoonShine\Decorations\Block; // [tl! focus]
 
 //...
 public function fields(): array
 {
     return [
-        Block::make('Block title', [
+        Block::make('Block title', [ // [tl! focus]
             Text::make('Имя', 'first_name'),
-        ]),
+        ]), // [tl! focus]
     ];
 }
 //...
 </x-code>
+
+<x-image theme="light" src="{{ asset('screenshots/block.png') }}"></x-image>
+<x-image theme="dark" src="{{ asset('screenshots/block_dark.png') }}"></x-image>
 
 <x-p>
     Если не нужен заголовок
 </x-p>
 
 <x-code language="php">
-use MoonShine\Decorations\Block;
+use MoonShine\Decorations\Block; // [tl! focus]
 
 //...
 public function fields(): array
 {
     return [
-        Block::make([
+        Block::make([ // [tl! focus]
             Text::make('Имя', 'first_name'),
-        ]),
+        ]), // [tl! focus]
     ];
 }
 //...
 </x-code>
+
+<x-image theme="light" src="{{ asset('screenshots/block_without_title.png') }}"></x-image>
+<x-image theme="dark" src="{{ asset('screenshots/block_without_title_dark.png') }}"></x-image>
+
 </x-page>
