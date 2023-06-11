574b4279f404460c669c288e91bcf5b4a0bf87c3
Alexander Nikushkin <nikushkin@mail.ru>
2023-06-11 17:46:32 +0500

    docs(fields): async search relations

--- resources/views/pages/en/fields/shared/async_search.blade.php
+++ resources/views/pages/en/fields/shared/async_search.blade.php
@@ -4,9 +4,35 @@
     Async search for values is provided by the <code>asyncSearch()</code> method.
 </x-p>
 
+<x-code language="php">
+use MoonShine\Fields\{{ $field }}; // [tl! focus]
+
+//...
+public function fields(): array
+{
+    return [
+        {{ $field }}::make('Contacts') // [tl! focus]
+            ->asyncSearch(){!!$field === 'BelongsToMany' ? "->fields([Text::make('Contact', 'text')])" : ""!!} // [tl! focus]
+    ];
+}
+//...
+</x-code>
+
+<x-moonshine::alert type="default" icon="heroicons.information-circle">
+    The search will be performed on the resource relation field <code>titleField</code>. The default is <code>titleField=id</code>.
+</x-moonshine::alert>
+
 <x-p>
-    The <code>asyncSearchQuery()</code> parameter is used to filter values,
-    and <code>asyncSearchValueCallback()</code> to customize the output
+    You can pass parameters to the <code>asyncSearch()</code> method:
+</x-p>
+
+<x-p>
+    <ul>
+        <li><code>title</code> - search field</li>
+        <li><code>count</code> - amount of items in output</li>
+        <li><code>asyncSearchQuery()</code> - callback function for filtering values</li>
+        <li><code>asyncSearchValueCallback()</code> - callback function for output customization.</li>
+    </ul>
 </x-p>
 
 <x-code language="php">
@@ -18,22 +44,23 @@ public function fields(): array
 {
     return [
         {{ $field }}::make('Contacts')->asyncSearch(
-            'title', // search column
+            'title',
+            10,
             asyncSearchQuery: function (Builder $query) {
                 return $query->where('id', '!=', 2);
             },
             asyncSearchValueCallback: function ($contact) {
                 return $contact->id . ' | ' . $contact->title;
             }
-        ){!!$field === 'BelongsToMany' ? "->fields([Text::make('Contact', 'text')])" : ""!!} // [tl! focus:-8]
+        ){!!$field === 'BelongsToMany' ? "->fields([Text::make('Contact', 'text')])" : ""!!} // [tl! focus:-9]
     ];
 }
 //...
 </x-code>
 
 <x-p>
-    When building a request for <code>asyncSearchQuery()</code>, you can use <code>Request</code>,
-    to do this, you need to pass it to the callback function
+    When building a query in <code>asyncSearchQuery()</code>, you can use the current form values.
+    To do this, you need to pass <code>Request</code> to the callback functions.
 </x-p>
 
 <x-code language="php">
