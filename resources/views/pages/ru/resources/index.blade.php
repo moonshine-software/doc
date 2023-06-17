b9b5bf386f2f241388a9b46a1580d0b0002a6bb2
DissNik <nikushkin@mail.ru>
2023-06-17 17:42:23 +0500

    docs(resources): resolve routes

--- resources/views/pages/ru/resources/index.blade.php
+++ resources/views/pages/ru/resources/index.blade.php
@@ -184,6 +184,10 @@ class MoonShineServiceProvider extends ServiceProvider
 
 <x-sub-title id="routes">Маршруты ресурса</x-sub-title>
 
+<x-p>
+    В MoonShine у ресурса для различных действий зарегистрировано множество маршрутов:
+</x-p>
+
 <x-code language="php">
 $this->route('index'); // GET|HEAD - список записей
 $this->route('create'); // GET|HEAD - создание новой записи
@@ -195,6 +199,42 @@ $this->route('show', $resourceItem); // GET|HEAD - просмотр записи
 $this->route('query-tag', $queryTag); // GET|HEAD - список записей с применением быстрого фильтра / тега
 $this->route('update-column', $resourceItem); // PUT - сохранение поля записи
 </x-code>
+
+<x-p>
+    Через метод <code>resolveRoutes()</code> можно добавить или переопределять роуты по умолчанию.
+
+<x-code language="php">
+namespace App\MoonShine\Resources;
+
+use App\Models\Post;
+use MoonShine\Resources\Resource;
+
+class PostResource extends Resource
+{
+    public static string $model = Post::class;
+
+    // ...
+
+    public function resolveRoutes(): void // [tl! focus:start]
+    {
+        parent::resolveRoutes();
+
+        Route::prefix('resource')->group(function (): void {
+            Route::get("{$this->uriKey()}/restore/{resourceItem}", function (Post $item) {
+                $item->restore();
+
+                return redirect()->back();
+            });
+        });
+    } // [tl! focus:end]
+
+    // ...
+}
+</x-code>
+
+<x-moonshine::alert type="default" icon="heroicons.book-open">
+    Для доступа к маршруту вне ресурса можно следующим способом <code>(new Resource())->route('index')</code>.
+</x-moonshine::alert>
 
 <x-sub-title id="modal">Модальные окна</x-sub-title>
 
