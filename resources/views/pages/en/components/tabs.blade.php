99bb11579361d4d1730bb6a6c2d1fdc1c7cae691
Alexander Nikushkin <nikushkin@mail.ru>
2023-08-29 09:48:28 +0500

    docs(components): tabs component active tab

--- resources/views/pages/en/components/tabs.blade.php
+++ resources/views/pages/en/components/tabs.blade.php
@@ -1,4 +1,11 @@
-<x-page title="Tabs" :sectionMenu="[]">
+<x-page title="Tabs" :sectionMenu="[
+    'Разделы' => [
+        ['url' => '#basics', 'label' => 'Basics'],
+        ['url' => '#active', 'label' => 'Active tab'],
+    ]
+]">
+
+<x-sub-title id="basics">Basics</x-sub-title>
 
 <x-p>
     To create tabs, you can use the <code>moonshine::tabs</code> component
@@ -10,6 +17,22 @@
     <x-moonshine::column adaptiveColSpan="12" colSpan="6">
         <x-moonshine::box>
             @include("examples/components/tabs")
+        </x-moonshine::box>
+    </x-moonshine::column>
+</x-moonshine::grid>
+
+<x-sub-title id="active">Active tab</x-sub-title>
+
+<x-p>
+    You can specify the default active tab by setting the <code>activeTab</code>.
+</x-p>
+
+<x-code language="blade" file="resources/views/examples/components/tabs-active.blade.php"></x-code>
+
+<x-moonshine::grid>
+    <x-moonshine::column adaptiveColSpan="12" colSpan="6">
+        <x-moonshine::box>
+            @include("examples/components/tabs-active")
         </x-moonshine::box>
     </x-moonshine::column>
 </x-moonshine::grid>
--- resources/views/pages/ru/components/tabs.blade.php
+++ resources/views/pages/ru/components/tabs.blade.php
@@ -1,4 +1,11 @@
-<x-page title="Tabs" :sectionMenu="[]">
+<x-page title="Tabs" :sectionMenu="[
+    'Разделы' => [
+        ['url' => '#basics', 'label' => 'Основы'],
+        ['url' => '#active', 'label' => 'Активная вкладка'],
+    ]
+]">
+
+<x-sub-title id="basics">Основы</x-sub-title>
 
 <x-p>
     Для создания вкладок можно воспользоваться компонентом <code>moonshine::tabs</code>
@@ -10,6 +17,22 @@
     <x-moonshine::column adaptiveColSpan="12" colSpan="6">
         <x-moonshine::box>
             @include("examples/components/tabs")
+        </x-moonshine::box>
+    </x-moonshine::column>
+</x-moonshine::grid>
+
+<x-sub-title id="active">Активная вкладка</x-sub-title>
+
+<x-p>
+    Указать активную вкладку по умолчанию, можно задав <code>activeTab</code>.
+</x-p>
+
+<x-code language="blade" file="resources/views/examples/components/tabs-active.blade.php"></x-code>
+
+<x-moonshine::grid>
+    <x-moonshine::column adaptiveColSpan="12" colSpan="6">
+        <x-moonshine::box>
+            @include("examples/components/tabs-active")
         </x-moonshine::box>
     </x-moonshine::column>
 </x-moonshine::grid>
