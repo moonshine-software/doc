3e9195a05e9f7a1734a7d19c0ca4cb6192295e76
Alexander Nikushkin <nikushkin@mail.ru>
2023-09-17 17:04:03 +0500

    docs(resources): query tags

--- resources/views/pages/ru/resources/query_tags.blade.php
+++ resources/views/pages/ru/resources/query_tags.blade.php
@@ -1,4 +1,12 @@
-<x-page title="Быстрые фильтры/Теги" :sectionMenu="[]">
+<x-page title="Быстрые фильтры/Теги" :sectionMenu="[
+    'Разделы' => [
+        ['url' => '#basics', 'label' => 'Основы'],
+        ['url' => '#icon', 'label' => 'Иконка'],
+        ['url' => '#can-see', 'label' => 'Условие отображения'],
+    ]
+]">
+
+<x-sub-title id="basics">Основы</x-sub-title>
 
 <x-p>
     Иногда возникает потребность создать набор фильтров (подборку результатов)
@@ -6,33 +14,29 @@
 </x-p>
 
 <x-code language="php">
-namespace MoonShine\Resources;
+namespace App\MoonShine\Resources;
 
-use MoonShine\Models\MoonshineUser;
+use App\Models\Post;
 use MoonShine\QueryTags\QueryTag; // [tl! focus]
-use Illuminate\Contracts\Database\Eloquent\Builder;
+use MoonShine\Resources\ModelResource;
 
-class PostResource extends Resource
+class PostResource extends ModelResource
 {
-    public static string $model = App\Models\Post::class;
+    protected string $model = Post::class;
 
-    public static string $title = 'Статьи';
+    protected string $title = 'Posts';
+
     //...
 
     public function queryTags(): array // [tl! focus:start]
     {
         return [
             QueryTag::make(
-                'Post with author', // Заголовок тега
+                'Post with author', // Tag Title
                 fn(Builder $query) => $query->whereNotNull('author_id') // Query builder
-            ),
-
-            QueryTag::make(
-                'Post without an author',
-                fn(Builder $query) => $query->whereNull('author_id')
-            )->icon('users')
+            )
         ];
-    } // [tl! focus:end]
+    } // [tl! focus:end] // [tl! focus:end]
 
     //...
 }
@@ -40,5 +44,81 @@ class PostResource extends Resource
 
 <x-image theme="light" src="{{ asset('screenshots/query_tags.png') }}"></x-image>
 <x-image theme="dark" src="{{ asset('screenshots/query_tags_dark.png') }}"></x-image>
+
+<x-sub-title id="icon">Иконка</x-sub-title>
+
+<x-p>
+    Тега можно добавить иконку воспользовавшись методом <code>icon()</code>.
+</x-p>
+
+<x-code language="php">
+namespace App\MoonShine\Resources;
+
+use App\Models\Post;
+use MoonShine\QueryTags\QueryTag;
+use MoonShine\Resources\ModelResource;
+
+class PostResource extends ModelResource
+{
+    protected string $model = Post::class;
+
+    protected string $title = 'Posts';
+
+    //...
+
+    public function queryTags(): array
+    {
+        return [
+            QueryTag::make(
+                'Post without an author',
+                fn(Builder $query) => $query->whereNull('author_id')
+            )->icon('heroicons.users') // [tl! focus]
+        ];
+    }
+
+    //...
+}
+</x-code>
+
+<x-moonshine::alert type="default" icon="heroicons.book-open">
+    За более подробной информацией обратитесь к разделу <x-link link="{{ route('moonshine.custom_page', 'icons-index') }}">Icons</x-link>
+</x-moonshine::alert>
+
+<x-sub-title id="can-see">Условие отображения</x-sub-title>
+
+<x-p>
+    Может потребоваться отобразить теги только при определенных условиях,
+    для этого можно воспользоваться методом <code>canSee()</code>,
+    которому необходимо передать callback функцию возвращающее <code>TRUE</code> или <code>FALSE</code>.
+</x-p>
+
+<x-code language="php">
+namespace App\MoonShine\Resources;
+
+use App\Models\Post;
+use MoonShine\QueryTags\QueryTag;
+use MoonShine\Resources\ModelResource;
+
+class PostResource extends ModelResource
+{
+    protected string $model = Post::class;
+
+    protected string $title = 'Posts';
+
+    //...
+
+    public function queryTags(): array
+    {
+        return [
+            QueryTag::make(
+                'Post with author', // Заголовок тега
+                fn(Builder $query) => $query->whereNotNull('author_id')
+            )->canSee(fn() => auth()->user()->moonshine_user_role_id === 1) // [tl! focus]
+        ];
+    }
+
+    //...
+}
+</x-code>
 
 </x-page>
