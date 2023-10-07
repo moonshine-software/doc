3fa4e32e6deebd60727e3c06d3c1ad0b6fb55d97
Alexander Nikushkin <nikushkin@mail.ru>
2023-10-07 21:02:52 +0500

    refactor(resources): query

--- resources/views/pages/ru/resources/query.blade.php
+++ resources/views/pages/ru/resources/query.blade.php
@@ -1,18 +1,18 @@
 <x-page title="Query" :sectionMenu="[
     'Разделы' => [
-        ['url' => '#filter', 'label' => 'Filter'],
-        ['url' => '#order', 'label' => 'Order'],
+        ['url' => '#query', 'label' => 'Запрос'],
+        ['url' => '#item-query', 'label' => 'Получение записи'],
+        ['url' => '#search', 'label' => 'Поиск'],
+        ['url' => '#order', 'label' => 'Сортировка'],
     ]
 ]">
 
-<x-sub-title id="filter">Filter</x-sub-title>
+
+<x-sub-title id="filter">Запрос</x-sub-title>
 
 <x-p>
-    Часто необходимо изначально отфильтровать выдачу со списком записей.
-</x-p>
-
-<x-p>
-    В целом, вы можете легко переопределить query builder в ресурсе.
+    Часто необходимо изначально изменить все запросы ресурса к базе данных.<br />
+    Вы можете легко переопределить <em>query builder</em> в ресурсе.
 </x-p>
 
 <x-code language="php">
@@ -39,12 +39,73 @@ class PostResource extends ModelResource
 }
 </x-code>
 
-<x-sub-title id="order">Order</x-sub-title>
+<x-sub-title id="item-query">Получение записи</x-sub-title>
 
 <x-p>
-    Переопределив метод <code>resolveOrder()</code> можно кастомизировать сортировку элементов.<br />
-    Метод принимать в качестве параметров столбец по которому производится сортировка
-    и направление (по возрастанию или убыванию)
+    Метод <code>resolveItemQuery()</code> используется,
+    если требуется изменить запрос на получение записи из базы данных.
+</x-p>
+
+<x-code language="php">
+namespace App\MoonShine\Resources;
+
+use App\Models\Post;
+use MoonShine\Resources\ModelResource;
+
+class PostResource extends ModelResource
+{
+    protected string $model = Post::class;
+
+    protected string $title = 'Posts';
+
+    //...
+
+    public function resolveItemQuery(): Builder // [tl! focus:start]
+    {
+        return parent::resolveItemQuery()
+            ->withTrashed();
+    } // [tl! focus:end]
+
+    //...
+}
+</x-code>
+
+<x-sub-title id="search">Поиск</x-sub-title>
+
+<x-p>
+    Метод <code>searchQuery()</code> позволяет изменить запрос при поиске записей.
+</x-p>
+
+<x-code language="php">
+namespace App\MoonShine\Resources;
+
+use App\Models\Post;
+use MoonShine\Resources\ModelResource;
+
+class PostResource extends ModelResource
+{
+    protected string $model = Post::class;
+
+    protected string $title = 'Posts';
+
+    //...
+
+    public function searchQuery(): Builder // [tl! focus:start]
+    {
+        return parent::searchQuery()
+            ->withTrashed();
+    } // [tl! focus:end]
+
+    //...
+}
+</x-code>
+
+<x-sub-title id="order">Сортировка</x-sub-title>
+
+<x-p>
+    Переопределив метод <code>resolveOrder()</code>, можно кастомизировать сортировку записей.<br />
+    Метод принимать в качестве параметров поле по которому производится сортировка
+    и направление (по возрастанию или убыванию).
 </x-p>
 
 <x-code language="php">
