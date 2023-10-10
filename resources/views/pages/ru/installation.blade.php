16aa9365cbcca740e16e42e0579b2b090daa98ac
Alexander Nikushkin <nikushkin@mail.ru>
2023-10-10 20:15:31 +0500

    chore: moonshine:2.0.0.alpha.2

--- resources/views/pages/ru/installation.blade.php
+++ resources/views/pages/ru/installation.blade.php
@@ -21,7 +21,7 @@
 <x-sub-title id="composer">Composer</x-sub-title>
 
 <x-code language="shell">
-    composer require moonshine/moonshine:2.0.0.alpha.2
+    composer require moonshine/moonshine:2.0.0.alpha.3
 </x-code>
 
 <x-sub-title id="install">Установка</x-sub-title>
