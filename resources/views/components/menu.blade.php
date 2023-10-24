308ca63b3e9c88d95a6807265e3b507299e89d67
Aleksandr Medvedev <s@lifestar.ru>
2023-10-24 19:43:38 +0300

    Added lovely sticky for sections menu

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,14 +1,14 @@
 @props([
     'data' => []
 ])
-<ul>
+<ul class="sticky-menu">
     @foreach($data as $group => $items)
         <li class="space-y-2">
             <div>{{ $group }}</div>
             <ul class="inline-flex gap-2 flex-wrap">
                 @foreach($items as $item)
                     <li>
-                        <x-moonshine::link-button href="{{ $item['url'] }}">
+                        <x-moonshine::link-button href="{{ $item['url'] }}" @click.prevent="scrollToSection('{{$item['url']}}')">
                             {{ $item['label'] }}
                         </x-moonshine::link>
                     </li>
