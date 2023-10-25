6f41d559a3ba249cf53a4ab3d9a5c463bea67ddf
Alexander Nikushkin <nikushkin@mail.ru>
2023-10-25 08:18:45 +0500

    style: take into account the height of the sticky menu block in ScrollToSection

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,13 +1,14 @@
 function stopVideo($elements) {
-    document.querySelectorAll($elements).forEach(function(video) {
+    document.querySelectorAll($elements).forEach(function (video) {
         video.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', "*")
     })
 }
 
 function scrollToSection(sectionId) {
     window.scrollTo({
-        // 88 - высота блока Разделы с кнопками
-        top: document.querySelector(sectionId).getBoundingClientRect().top + window.scrollY - 88,
+        top: document.querySelector(sectionId).getBoundingClientRect().top
+            + window.scrollY
+            - document.querySelector('.sticky-menu').offsetHeight ?? 0,
         behavior: 'smooth'
     })
 }
