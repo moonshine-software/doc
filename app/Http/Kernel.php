019c58c1a73d62b448b7818217d96e3445fdbd2f
LT <leetoplayaz@yandex.ru>
2023-11-03 21:06:08 +0300

    fix: assets load

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        RenderTorchlight::class
     ];
 
     /**
@@ -37,7 +38,6 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            RenderTorchlight::class
         ],
 
         'api' => [
