08d2b1efbbc3273996116dcc339c622db9a6f088
Alexander Nikushkin <nikushkin@mail.ru>
2023-11-06 19:54:41 +0500

    docs(fields): TinyMce LFM

--- resources/views/pages/ru/fields/tinymce.blade.php
+++ resources/views/pages/ru/fields/tinymce.blade.php
@@ -85,7 +85,7 @@ public function fields(): array
 </x-code>
 
 <x-moonshine::alert type="default" icon="heroicons.information-circle">
-    Файлы переводов размещаются в директории <code>public/vendor/moonshine/libs/tinymce/langs</code>
+    Файлы переводов размещаются в директории <code>public/vendor/moonshine/libs/tinymce/langs</code>.
 </x-moonshine::alert>
 
 <x-sub-title id="custom-config">Дополнительные настройки</x-sub-title>
@@ -117,12 +117,11 @@ public function fields(): array
 <x-sub-title id="filemanager">File manager</x-sub-title>
 
 <x-p>
-    Если вы хотите использовать файловый менеджер в <em>TinyMce</em>, то вам необходимо установить пакет Laravel FileManager.
+    Если вы хотите использовать файловый менеджер в <em>TinyMce</em>, то вам необходимо установить пакет
+    <x-link link="https://github.com/UniSharp/laravel-filemanager" target="_blank">Laravel FileManager</x-link>.
 </x-p>
 
-<x-link link="https://github.com/UniSharp/laravel-filemanager" target="_blank">Laravel FileManager</x-link>
-
-<x-sub-title hashtag="1">Установка</x-sub-title>
+<x-moonshine::divider label="Установка" />
 
 <x-code language="shell">
 composer require unisharp/laravel-filemanager
@@ -145,31 +144,82 @@ return [
 ];
 </x-code>
 
-<x-sub-title hashtag="2">Add routes to the app/Providers/RouteServiceProvider.php</x-sub-title>
+<x-moonshine::divider label="Файл маршрутов" />
+
+<x-p>
+    Создайте файл маршрутов, например <code>routes/moonshine.php</code>
+    и зарегистрируйте маршруты <em>LaravelFilemanager</em>.
+</x-p>
+
+<x-code language="php">
+use Illuminate\Support\Facades\Route;
+use UniSharp\LaravelFilemanager\Lfm;
+
+Route::prefix('laravel-filemanager')->group(function () {
+    Lfm::routes();
+});
+</x-code>
+
+<x-moonshine::divider label="Регистрация файла" />
+
+<x-p>
+    Зарегистрируйте созданный файл маршрутов в <code>app/Providers/RouteServiceProvider.php</code>.
+</x-p>
+
+<x-moonshine::alert type="default" icon="heroicons.information-circle">
+    Файл маршрутов обязательно должен быть в группе middleware <code>moonshine</code>!
+</x-moonshine::alert>
 
 <x-code language="php">
 // ...
 
-Route::middleware('web')
-    ->group(base_path('routes/web.php'));
+public function boot()
+{
+    // ...
 
-Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['moonshine', 'auth.moonshine']], function () {
-    UniSharp\LaravelFilemanager\Lfm::routes();
-}); // [tl! focus:-2]
+    $this->routes(function () {
+        // ...
+
+        Route::middleware('moonshine')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/moonshine.php')); // [tl! focus:-2]
+    });
+}
 
 // ...
 </x-code>
 
 <x-moonshine::alert type="default" icon="heroicons.information-circle">
-    Роут файлового менеджера обязательно должен быть в группе middleware <code>moonshine</code>, а не в web!
-</x-moonshine::alert>
-
-<x-moonshine::alert type="default" icon="heroicons.information-circle">
     Для того чтобы разрешить доступ только авторизованным в админ-панели пользователям
-    необходимо использовать middleware <code>auth.moonshine</code>
+    необходимо добавить middleware <code>MoonShine\Http\Middleware\Authenticate</code>.
 </x-moonshine::alert>
 
-<x-sub-title hashtag="3">Добавьте префикс в config/moonshine.php</x-sub-title>
+<x-code language="php">
+use MoonShine\Http\Middleware\Authenticate; // [tl! focus]
+
+// ...
+
+public function boot()
+{
+    // ...
+
+    $this->routes(function () {
+        // ...
+
+        Route::middleware(['moonshine', Authenticate::class]) // [tl! focus]
+            ->namespace($this->namespace)
+            ->group(base_path('routes/moonshine.php'));
+    });
+}
+
+// ...
+</x-code>
+
+<x-moonshine::divider label="Конфигурация" />
+
+<x-p>
+    В файле конфигурации <code>config/moonshine.php</code> необходимо добавить префикс.
+</x-p>
 
 <x-code language="php">
 'tinymce' => [
