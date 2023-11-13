8e5917eba5369f1a5ee9457da0890ffd380c657d
Alexander Nikushkin <nikushkin@mail.ru>
2023-11-13 20:08:29 +0500

    docs(field): fix typos

--- resources/views/pages/ru/fields/shared/values_query.blade.php
+++ resources/views/pages/ru/fields/shared/values_query.blade.php
@@ -17,7 +17,7 @@ use MoonShine\Fields\Relationships\{{ $field }};
 public function fields(): array
 {
     return [
-        {{ $field }}::make({!! $field === 'BelongsToMany' ? 'Categories' : 'Category' !!}, resource: new CategoryResource())
+        {{ $field }}::make('{{ $field === 'BelongsToMany' ? 'Categories' : 'Category' }}', '{{ $field === 'BelongsToMany' ? 'categories' : 'category' }}', resource: new CategoryResource())
             ->valuesQuery(fn(Builder $query, Field $field) => $query->where('active', true)) // [tl! focus]
     ];
 }
