169808b78ead52b413f582653ffab39ceb5366d9
Alexander Nikushkin <nikushkin@mail.ru>
2023-11-13 14:46:20 +0500

    docs(fields): method placeholder()

---
+++ resources/views/pages/ru/fields/shared/placeholder.blade.php
@@ -0,0 +1,26 @@
+<x-sub-title id="placeholder">Placeholder</x-sub-title>
+
+<x-p>
+    Метод <code>placeholder()</code> позволяет задать у поля аттрибут <em>placeholder</em>.
+</x-p>
+
+<x-code language="php">
+placeholder(string $value)
+</x-code>
+
+<x-code language="php">
+use MoonShine\Fields\{!! ($field === 'BelongsTo' || $field === 'BelongsToMany') ? 'Relationships\\' : '' !!}{{ $field }};
+
+//...
+
+public function fields(): array
+{
+    return [
+        {{ $field }}::make({!! $field === 'BelongsToMany' ? "'Countries'" : "'Country'" !!}, {!! $field === 'BelongsToMany' ? "'countries'" : "'country'" !!})
+            ->nullable()
+            ->placeholder('{!! $field === 'BelongsToMany' ? 'Countries' : 'Country' !!}') // [tl! focus]
+    ];
+}
+
+//...
+</x-code>
