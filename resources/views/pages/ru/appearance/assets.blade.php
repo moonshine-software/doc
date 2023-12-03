a687a99a0c62100c3e2358014749e68d29e1690c
Alexander Nikushkin <nikushkin@mail.ru>
2023-12-03 23:10:50 +0500

    docs(appearance): moonShineAssets add vite asset

--- resources/views/pages/ru/appearance/assets.blade.php
+++ resources/views/pages/ru/appearance/assets.blade.php
@@ -25,6 +25,28 @@ class MoonShineServiceProvider extends MoonShineApplicationServiceProvider
 </x-code>
 
 <x-p>
+    Можно также добавлять свои Vite ассеты:
+</x-p>
+
+<x-code language="php">
+class MoonShineServiceProvider extends MoonShineApplicationServiceProvider
+{
+    //...
+
+    public function boot(): void
+    {
+        parent::boot();
+
+        moonShineAssets()->add([
+            Vite::asset('resources/js/app.js')
+        ]);  // [tl! focus:-3]
+    }
+
+    //...
+}
+</x-code>
+
+<x-p>
     Если Вы хотите использовать стили и скрипты <strong>MoonShine</strong> за пределами админ-панели,
     то необходимо подключить директиву <code>@@moonShineAssets</code>
 </x-p>
