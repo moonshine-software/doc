f5e1f04f0b35374bada397ccb1b01de75943c678
LT <leetoplayaz@yandex.ru>
2023-12-05 16:03:56 +0300

    chore: alert with 2.0

--- resources/views/components/page.blade.php
+++ resources/views/components/page.blade.php
@@ -3,6 +3,18 @@
     'videos' => [],
     'sectionMenu' => []
 ])
+<x-moonshine::alert type="success">
+    @if(app()->currentLocale() === 'ru')
+        Вы просматриваете документацию старой версии MoonShine.
+        Рассмотрите возможность обновления вашего проекта до
+    @else
+        You're browsing the documentation for an old version of MoonShine.
+        Consider upgrading your project to
+    @endif
+
+    <a href="https://moonshine-laravel.com">MoonShine 2.x.</a>
+</x-moonshine::alert>
+
 <x-video :data="$videos"/>
 <x-menu :data="$sectionMenu"></x-menu>
 <div>
