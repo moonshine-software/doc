42f0a4382fa0f2d8ace12596b7528b0f8b6f40eb
Alexander Nikushkin <nikushkin@mail.ru>
2023-12-15 19:46:14 +0500

    docs(components): Modal

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home')->name('home');
 
-Route::get('async', function (){
+Route::match(['get', 'post'], 'async', function (){
    return fake()->text();
 })->name('async');
