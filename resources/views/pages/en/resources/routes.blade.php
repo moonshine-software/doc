38f21dbc442708691f319b72d7734f258b7afed7
Alexander Nikushkin <nikushkin@mail.ru>
2023-12-25 19:17:09 +0500

    docs(resources): routes through helper

--- resources/views/pages/en/resources/routes.blade.php
+++ resources/views/pages/en/resources/routes.blade.php
@@ -1,8 +1,11 @@
-<x-page title="Routes" :sectionMenu="[]">
+<x-page title="Routes" :sectionMenu="[
+    'Sections' => [
+        ['url' => '#basics', 'label' => 'Basics'],
+        ['url' => '#helper', 'label' => 'Helper'],
+    ]
+]">
 
-<x-p>
-    Sugar for routes:
-</x-p>
+<x-sub-title id="basics">Basics</x-sub-title>
 
 <x-code language="php">
 $resource->url(); // First page of the resource
@@ -40,6 +43,39 @@ $page->route($params);  // Advanced method for obtaining routes
 $page->asyncMethodUrl('updateSomething'); // ANY
 
 $page->fragmentLoadUrl('table-index');
+</x-code>
+
+<x-sub-title id="helper">Helper</x-sub-title>
+
+<x-p>
+    You can also use the <code>to_page</code> helper:
+</x-p>
+
+<x-code language="php">
+to_page(
+    string|Page|null $page = null,
+    string|ResourceContract|null $resource = null,
+    array $params = [],
+    bool $redirect = false,
+    ?string $fragment = null
+)
+</x-code>
+
+<x-ul>
+    <li><code>$page</code> - page or page alias (optional),</li>
+    <li><code>$resource</code> - resource or resource alias (optional),</li>
+    <li><code>$params</code> - additional query,</li>
+    <li><code>$redirect</code> - if necessary, perform a redirect immediately,</li>
+    <li><code>$fragment</code> - url will be used for Fragment loading.</li>
+</x-ul>
+
+<x-code>
+to_page(page: 'form-page');
+to_page(page: IndexPage::class);
+to_page(page: IndexPage::class, resource: PostResource::class);
+to_page(page: new IndexPage(), resource: new PostResource());
+to_page(page: 'form-page', redirect: true);
+to_page(page: 'form-page', fragment: true);
 </x-code>
 
 </x-page>
