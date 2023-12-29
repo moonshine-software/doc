fb808d6e1247e102fc1ef8427f275b2ac463eb53
Alexander Nikushkin <nikushkin@mail.ru>
2023-12-29 21:17:53 +0500

    docs(installation): composer version

--- resources/views/pages/ru/installation.blade.php
+++ resources/views/pages/ru/installation.blade.php
@@ -17,7 +17,7 @@
     Для использования MoonShine необходимо выполнение следующих требований перед установкой:
 </x-p>
 
-<x-ul :items="['php >=8.1', 'laravel >= 10.23', 'composer']"></x-ul>
+<x-ul :items="['php >= 8.1', 'laravel >= 10.23', 'composer > 2']"></x-ul>
 
 <x-sub-title id="composer">Composer</x-sub-title>
 
