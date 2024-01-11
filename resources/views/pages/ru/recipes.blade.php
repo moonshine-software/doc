d1cce7ab1e2d8bf8ec3722386c46f0af10492cd6
Alexander Nikushkin <nikushkin@mail.ru>
2024-01-11 20:27:04 +0500

    docs(page): breadcrumbs

--- resources/views/pages/ru/recipes.blade.php
+++ resources/views/pages/ru/recipes.blade.php
@@ -7,6 +7,7 @@
             ['url' => '#assets-vite', 'label' => 'Подключение Vite build'],
             ['url' => '#custom-buttons', 'label' => 'Кастомные кнопки'],
             ['url' => '#hasone-through-template', 'label' => 'HasOne через поле Template'],
+            ['url' => '#custom-breadcrumbs', 'label' => 'Изменение breadcrumbs из ресурса'],
         ]
     ]"
 >
@@ -16,5 +17,6 @@
 @include('pages.ru.recipes.assets-vite', ['title' => 'Подключение Vite build'])
 @include('pages.ru.recipes.custom-buttons', ['title' => 'Кастомные кнопки'])
 @include('pages.ru.recipes.hasone-through-template', ['title' => 'HasOne через поле Template'])
+@include('pages.ru.recipes.custom-breadcrumbs', ['title' => 'Изменение breadcrumbs из ресурса'])
 
 </x-page>
