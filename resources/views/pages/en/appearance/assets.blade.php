f0cb5c6d68e9c2a931ba431f4bc8e3203ec4eecb
Alexander Nikushkin <nikushkin@mail.ru>
2024-03-22 20:54:01 +0500

    docs(appearance): assets

--- resources/views/pages/en/appearance/assets.blade.php
+++ resources/views/pages/en/appearance/assets.blade.php
@@ -1,8 +1,22 @@
-<x-page>
+<x-page title="Assets" :sectionMenu="[
+    'Sections' => [
+        ['url' => '#global', 'label' => 'Global assets'],
+        ['url' => '#resource-page', 'label' => 'Assets for the resource/page'],
+        ['url' => '#vite', 'label' => 'Vite'],
+        ['url' => '#config', 'label' => 'Configuration'],
+        ['url' => '#directive', 'label' => 'Directive'],
+    ]
+]">
 
 <x-p>
     You can connect any of your <em>css</em> and <em>js</em> files to MoonShine.
-    To do this, you need to add them to <code>MoonShineServiceProvider</code>.
+</x-p>
+
+<x-sub-title id="global">Global assets</x-sub-title>
+
+<x-p>
+    If you need to publish assets globally for all pages,
+    then you can add them to <code>MoonShineServiceProvider</code>.
 </x-p>
 
 <x-code language="php">
@@ -17,18 +31,53 @@ class MoonShineServiceProvider extends MoonShineApplicationServiceProvider
         moonShineAssets()->add([
             '/css/style.css',
             '/js/main.js',
-        ]);  // [tl! focus:-3]
+        ]); // [tl! focus:-3]
     }
 
     //...
 }
 </x-code>
 
+<x-sub-title id="resource-page">Assets for a resource/page</x-sub-title>
+
+<x-p>
+    Assets can be added for a resource or for a separate page,
+    To do this, you need to specify the <code>$assets</code> property.
+</x-p>
+
+<x-code language="php">
+class Post extends ModelResource
+{
+    protected array $assets = [
+        '/css/style.css',
+        '/js/main.js',
+    ]; // [tl! focus:-3]
+
+    //...
+}
+</x-code>
+
+<x-code language="php">
+class MyPage extends Page
+{
+    protected array $assets = [
+        '/css/style.css',
+        '/js/main.js',
+    ]; // [tl! focus:-3]
+
+    //...
+}
+</x-code>
+
+<x-sub-title id="vite">Vite</x-sub-title>
+
 <x-p>
     You can also add your own Vite assets:
 </x-p>
 
 <x-code language="php">
+use Illuminate\Support\Facades\Vite; // [tl! focus]
+
 class MoonShineServiceProvider extends MoonShineApplicationServiceProvider
 {
     //...
@@ -38,13 +87,45 @@ class MoonShineServiceProvider extends MoonShineApplicationServiceProvider
         parent::boot();
 
         moonShineAssets()->add([
-            Vite::asset('resources/js/app.js')
-        ]);  // [tl! focus:-3]
+            Vite::asset('resources/js/app.js') // [tl! focus]
+        ]);
     }
 
     //...
 }
 </x-code>
+
+<x-sub-title id="config">Configuration</x-sub-title>
+
+<x-p>
+    You can configure the connection of assets in the configuration file <code>config/moonshine.php</code>
+</x-p>
+
+<x-code language="php">
+// ...
+
+return [
+    // ...
+
+    'assets' => [
+        'js' => [
+            'script_attributes' => [
+                'defer',
+                'type' => 'module'
+            ]
+        ],
+        'css' => [
+            'link_attributes' => [
+                'rel' => 'stylesheet'
+            ]
+        ]
+    ], // [tl! focus:-12]
+
+    // ...
+];
+</x-code>
+
+<x-sub-title id="directive">Directive</x-sub-title>
 
 <x-p>
     If you want to use <strong>MoonShine</strong> styles and scripts outside the admin panel,
@@ -57,5 +138,4 @@ class MoonShineServiceProvider extends MoonShineApplicationServiceProvider
 </head>
 </x-code>
 
-@include('pages.en.recipes.assets-vite')
 </x-page>
