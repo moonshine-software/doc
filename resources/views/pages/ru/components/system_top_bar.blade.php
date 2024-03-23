4b82e8a9b455d88960a655d228c480e1d36569b9
Alexander Nikushkin <nikushkin@mail.ru>
2024-03-23 20:56:14 +0500

    docs(components): hideLogo and hideSwitcher methods for TopBar

--- resources/views/pages/ru/components/system_top_bar.blade.php
+++ resources/views/pages/ru/components/system_top_bar.blade.php
@@ -4,6 +4,8 @@
         'Разделы' => [
             ['url' => '#make', 'label' => 'Make'],
             ['url' => '#actions', 'label' => 'Actions'],
+            ['url' => '#hide-logo', 'label' => 'Скрыть логотип'],
+            ['url' => '#hide-switcher', 'label' => 'Скрыть переключатель темы'],
         ]
     ]"
 >
@@ -95,5 +97,77 @@ final class MoonShineLayout implements MoonShineLayoutContract
 
 <x-image theme="light" src="{{ asset('screenshots/topbar_actions.png') }}"></x-image>
 <x-image theme="dark" src="{{ asset('screenshots/topbar_actions_dark.png') }}"></x-image>
+
+<x-sub-title id="hide-logo">Скрыть логотип</x-sub-title>
+
+<x-p>
+    Метод <code>hideLogo()</code> позволяет скрыть логотип.
+</x-p>
+
+<x-code language="php">
+hideLogo()
+</x-code>
+
+<x-code language="php">
+namespace App\MoonShine;
+
+use MoonShine\Components\Layout\LayoutBlock;
+use MoonShine\Components\Layout\LayoutBuilder;
+use MoonShine\Components\Layout\Menu;
+use MoonShine\Components\Layout\Profile;
+use MoonShine\Components\Layout\TopBar;
+use MoonShine\Contracts\MoonShineLayoutContract;
+
+final class MoonShineLayout implements MoonShineLayoutContract
+{
+    public static function build(): LayoutBuilder
+    {
+        return LayoutBuilder::make([
+            TopBar::make([
+                Menu::make()->top(),
+            ])
+                ->hideLogo(), // [tl! focus]
+
+            //...
+        ]);
+    }
+}
+</x-code>
+
+<x-sub-title id="hide-switcher">Скрыть переключатель темы</x-sub-title>
+
+<x-p>
+    Метод <code>hideSwitcher()</code> позволяет скрыть переключатель темы.
+</x-p>
+
+<x-code language="php">
+    hideSwitcher()
+</x-code>
+
+<x-code language="php">
+namespace App\MoonShine;
+
+use MoonShine\Components\Layout\LayoutBlock;
+use MoonShine\Components\Layout\LayoutBuilder;
+use MoonShine\Components\Layout\Menu;
+use MoonShine\Components\Layout\Profile;
+use MoonShine\Components\Layout\TopBar;
+use MoonShine\Contracts\MoonShineLayoutContract;
+
+final class MoonShineLayout implements MoonShineLayoutContract
+{
+    public static function build(): LayoutBuilder
+    {
+        return LayoutBuilder::make([
+            TopBar::make([
+                Menu::make()->top(),
+            ])
+                ->hideSwitcher(), // [tl! focus]
+
+            //...
+        ]);
+    }
+}
+</x-code>
 
 </x-page>
