6277d6da43c552c8e143c2495abd077006f7a9e1
Bugo <cheetah@bk.ru>
2024-04-23 20:21:02 +0500

    Add some translations

--- resources/views/components/doc-search.blade.php
+++ resources/views/components/doc-search.blade.php
@@ -15,6 +15,49 @@
         appId: '{{ config('algolia.doc.id') }}',
         apiKey: '{{ config('algolia.doc.key')  }}',
         indexName: 'moonshine-laravel',
+        placeholder: '{{ __('Search docs') }}',
+        translations: {
+            button: {
+                buttonText: '{{ __('Search') }}',
+                buttonAriaLabel: '{{ __('Search') }}'
+            },
+            modal: {
+                searchBox: {
+                    resetButtonTitle: '{{ __('Clear the query') }}',
+                    resetButtonAriaLabel: '{{ __('Clear the query') }}',
+                    cancelButtonText: '{{ __('Cancel') }}',
+                    cancelButtonAriaLabel: '{{ __('Cancel') }}'
+                },
+                startScreen: {
+                    recentSearchesTitle: '{{ __('Recent') }}',
+                    noRecentSearchesText: '{{ __('No recent searches') }}',
+                    saveRecentSearchButtonTitle: '{{ __('Save this search') }}',
+                    removeRecentSearchButtonTitle: '{{ __('Remove this search') }}',
+                    favoriteSearchesTitle: '{{ __('Favorite') }}',
+                    removeFavoriteSearchButtonTitle: '{{ __('Remove this search from favorites') }}'
+                },
+                errorScreen: {
+                    titleText: '{{ __('Unable to fetch results') }}',
+                    helpText: '{{ __('You might want to check your network connection.') }}'
+                },
+                footer: {
+                    selectText: '{{ __('to select') }}',
+                    selectKeyAriaLabel: '{{ __('Enter key') }}',
+                    navigateText: '{{ __('to navigate') }}',
+                    navigateUpKeyAriaLabel: '{{ __('Arrow up') }}',
+                    navigateDownKeyAriaLabel: '{{ __('Arrow down') }}',
+                    closeText: '{{ __('to close') }}',
+                    closeKeyAriaLabel: '{{ __('Escape key') }}',
+                    searchByText: '{{ __('Search by') }}'
+                },
+                noResultsScreen: {
+                    noResultsText: '{{ __('No results for') }}',
+                    suggestedQueryText: '{{ __('Try searching for') }}',
+                    reportMissingResultsText: '{{ __('Believe this query should return results?') }}',
+                    reportMissingResultsLinkText: '{{ __('Let us know.') }}'
+                },
+            },
+        },
         container: '#doc-search',
         debug: false,
     });
