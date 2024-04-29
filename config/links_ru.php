340a279a426792851e4bac8eecbda323a1a917db
Alexander Nikushkin <nikushkin@mail.ru>
2024-04-29 17:26:22 +0500

    docs: links for different locales

--- config/links_ru.php
+++ config/links_ru.php
@@ -4,4 +4,5 @@ return [
     'chat' => 'https://t.me/moonshine_ru',
     'youtube' => 'https://youtube.com/c/CutCodeRu',
     'github' => 'https://github.com/moonshine-software/moonshine',
+    'screencasts' => 'https://youtube.com/playlist?list=PLTucyHptHtTnfDI18bZnYEgvJIFmW8fGy&si=DgnBSDcpP9FTAc8Q',
 ];
