8032cbbef67f9ef14122890248565b72f1d0bc11
Alexander Nikushkin <nikushkin@mail.ru>
2024-06-28 20:31:31 +0500

    docs(fields): title and blank methods for Url

--- resources/views/pages/en/fields/url.blade.php
+++ resources/views/pages/en/fields/url.blade.php
@@ -1,8 +1,19 @@
-<x-page title="Url">
+<x-page
+    title="Url"
+    :sectionMenu="[
+        'Sections' => [
+            ['url' => '#basics', 'label' => 'Basics'],
+            ['url' => '#title', 'label' => 'Title'],
+            ['url' => '#blank', 'label' => 'Blank'],
+        ]
+    ]"
+>
 
 <x-extendby :href="to_page('fields-text')">
     Text
 </x-extendby>
+
+<x-sub-title id="basics">Basics</x-sub-title>
 
 <x-p>
     The <em>Url</em> field is an extension of <em>Text</em>,
@@ -22,6 +33,44 @@ public function fields(): array
 }
 
 //...
+</x-code>
+
+<x-sub-title id="title">Title</x-sub-title>
+
+<x-p>
+    The <code>title()</code> method allows you to set the title of the link.
+</x-p>
+
+<x-code language="php">
+title(Closure $callback)
+</x-code>
+
+<x-code language="php">
+use MoonShine\Fields\Url;
+
+//...
+
+Url::make('Link')
+    ->title(fn(string $url, Url $ctx) => str($url)->limit(3)) // [tl! focus]
+</x-code>
+
+<x-sub-title id="blank">Blank</x-sub-title>
+
+<x-p>
+    The <code>blank()</code> method allows you to open a link in a new window.
+</x-p>
+
+<x-code language="php">
+blank()
+</x-code>
+
+<x-code language="php">
+use MoonShine\Fields\Url;
+
+//...
+
+Url::make('Link')
+    ->blank() // [tl! focus]
 </x-code>
 
 </x-page>
